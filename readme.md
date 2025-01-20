7bd99ce64364626b0d3f078c63dd59fbe05c75df
TomKing062 <yx42_44@126.com>
2025-01-20 14:36:35 +0800

    make recovery optional

--- readme.md
+++ readme.md
@@ -1,13 +1,16 @@
 ## resign
 
+This method works with both unlocked and locked BL, but wiping userdata might be still needed. (vmbeta change will affect data encryption)
+
 |                                               | android 9                                                    | android 10(+)                                                |
 | --------------------------------------------- | ------------------------------------------------------------ | ------------------------------------------------------------ |
-| ums312/ums512/ud710 (not-fused or public key) | splloader+uboot+sml+trustos+vbmeta+recovery+boot             | splloader+uboot+sml+trustos+teecfg+vbmeta+recovery+boot      |
-| ums312/ums512/ud710 (fused)                   | uboot+sml+trustos+vbmeta+recovery+boot<br />splloader need manually process with [CVE-2022-38691](https://github.com/TomKing062/CVE-2022-38691_38692) | uboot+sml+trustos+teecfg+vbmeta+recovery+boot<br />splloader need manually process with [CVE-2022-38691](https://github.com/TomKing062/CVE-2022-38691_38692) |
-| other cpu (not-fused or public key)           | splloader+uboot+sml+trustos+vbmeta+recovery+boot             | splloader+uboot+sml+trustos+teecfg+vbmeta+recovery+boot      |
+| ums312/ums512/ud710 (not-fused or public key) | splloader+uboot+sml+trustos+vbmeta+boot+(recovery)           | splloader+uboot+sml+trustos+teecfg+vbmeta+boot+(recovery)    |
+| ums312/ums512/ud710 (fused)                   | uboot+sml+trustos+vbmeta+boot+(recovery)<br />see note for splloader information | uboot+sml+trustos+teecfg+vbmeta+boot+(recovery)<br />see note for splloader information |
+| other cpu (not-fused or public key)           | splloader+uboot+sml+trustos+vbmeta+boot+(recovery)           | splloader+uboot+sml+trustos+teecfg+vbmeta+boot+(recovery)    |
 | other cpu (fused)                             | UNSUPPORTED                                                  | UNSUPPORTED                                                  |
 
-## Note
+Note for ums312/ums512/ud710 (fused)：
 
-1. This method works with both unlocked and locked BL, but wiping userdata might be still needed. (vmbeta change will affect data encryption?)
-2. ums312/ums512/ud710: on android 11(+), you can use [gen_spl-unlock](https://raw.githubusercontent.com/TomKing062/CVE-2022-38694_unlock_bootloader/info/gen_spl-unlock.c) with [CVE-2022-38691](https://github.com/TomKing062/CVE-2022-38691_38692)
+​	on android 9/10, you need IDA to get patched splloader ([guide](https://github.com/TomKing062/CVE-2022-38694_unlock_bootloader/wiki/patch_do_cboot%E2%80%90SPL#old-type)), then process with [CVE-2022-38691](https://github.com/TomKing062/CVE-2022-38691_38692)
+
+​	on android 11(+), you can use [gen_spl-unlock](https://raw.githubusercontent.com/TomKing062/CVE-2022-38694_unlock_bootloader/info/gen_spl-unlock.c) to get patched splloader, then process with [CVE-2022-38691](https://github.com/TomKing062/CVE-2022-38691_38692)
--- sign.sh
+++ sign.sh
@@ -1,21 +1,41 @@
-sign9() {
-    cd recovery
-    ../magiskboot unpack -n recovery.img
-    cd ../boot
-    ../magiskboot unpack -n boot.img
-    cp -f ../recovery/ramdisk.cpio ./
-    ../magiskboot repack -n boot.img
-    mv boot.img boot_bkp.img
-    mv new-boot.img boot.img
-    ../magiskboot cleanup
-    ./boot_patch_9.sh
-    cd ../vbmeta
-    #python avbtool add_hash_footer --image ../boot/patched.img --partition_name boot --partition_size 36700160 --key rsa4096_boot.pem --algorithm SHA256_RSA4096
-    ./sign_avb.sh boot ../boot/boot_bkp.img ../boot/patched.img
-    cd ..
-}
-
-cd work
+mkdir work
+busybox unzip -oq original.zip -d work
+mkdir -p boot/zzz
+mkdir -p vbmeta/keys
+mkdir output
+tar xzvf avbtool.tgz -C vbmeta/
+mv work/boot* boot/boot.img
+mv work/vbmeta* vbmeta/keys/vbmeta.img
+busybox unzip -oq magisk.apk -d boot/zzz
+mv main/boot_patch_9.sh boot/
+mv main/boot_patch_10.sh boot/
+mv main/sign_avb.sh vbmeta/
+git clone https://github.com/TomKing062/vendor_sprd_proprietories-source_packimage.git
+cp -a vendor_sprd_proprietories-source_packimage/sign_image/v2/prebuilt/* work/
+cp -a vendor_sprd_proprietories-source_packimage/sign_image/config-unisoc work/
+cp vendor_sprd_proprietories-source_packimage/sign_image/v2/sign_image_v2.sh work/
+gcc -o work/get-raw-image vendor_sprd_proprietories-source_packimage/sign_image/get-raw-image.c
+chmod +x work/*
+cd vendor_sprd_proprietories-source_packimage/sign_vbmeta
+make
+chmod +x generate_sign_script_for_vbmeta
+cp generate_sign_script_for_vbmeta ../../vbmeta/keys/
+cd ../../vbmeta/keys/
+./generate_sign_script_for_vbmeta vbmeta.img
+mv sign_vbmeta.sh ../
+mv padding.py ../
+cd ../..
+cp work/config-unisoc/rsa4096_boot.pem vbmeta/
+cp -f work/config-unisoc/rsa4096_boot_pub.bin vbmeta/keys/
+cp work/config-unisoc/rsa4096_vbmeta.pem vbmeta/
+chmod +x vbmeta/*
+sudo rm -rf /usr/bin/python
+sudo ln -s /usr/bin/python2 /usr/bin/python
+cd vbmeta
+./sign_vbmeta.sh
+python padding.py
+cp vbmeta-sign-custom.img ../output/vbmeta.img
+cd ../work
 
 if [ -f "splloader.bin" ]; then
     ./get-raw-image "splloader.bin"
@@ -62,26 +82,40 @@ elif [ -f "trustos.bin" ]; then
 fi
 
 if [ ! -f "teecfg.bin" ]; then
-    cd ..
-    sign9
+    cd ../boot
+    ./boot_patch_9.sh
 else
     ./get-raw-image "teecfg.bin"
     RETVAL=$?
     if [ $RETVAL -ne 0 ]; then
         rm teecfg.bin
-        cd ..
-        sign9
+        cd ../boot
+        ./boot_patch_9.sh
     else
         cd ../boot
         ./boot_patch_10.sh
-        cd ../vbmeta
-        #python avbtool add_hash_footer --image ../boot/patched.img --partition_name boot --partition_size 36700160 --key rsa4096_boot.pem --algorithm SHA256_RSA4096 --prop com.android.build.boot.os_version:10
-        ./sign_avb.sh boot ../boot/boot.img ../boot/patched.img
-        cd ..
     fi
 fi
 
-cd vbmeta
-#python avbtool add_hash_footer --image ../recovery/recovery.img --partition_name recovery --partition_size 41943040 --key rsa4096_recovery.pem --algorithm SHA256_RSA4096
-./sign_avb.sh recovery ../recovery/recovery.img ../recovery/recovery.img
+cd ../vbmeta
+./sign_avb.sh boot ../boot/boot.img ../boot/patched.img
+cp ../boot/patched.img ../output/boot.img
 cd ..
+
+mkdir recovery
+mv work/recovery* recovery/recovery.img
+RETVAL=$?
+if [ $RETVAL -eq 0 ]; then
+    cp work/config-unisoc/rsa4096_recovery.pem vbmeta/
+    cp -f work/config-unisoc/rsa4096_recovery_pub.bin vbmeta/keys/
+    cd vbmeta
+    ./sign_avb.sh recovery ../recovery/recovery.img ../recovery/recovery.img
+    cp ../recovery/recovery.img ../output/recovery.img
+    cd ..
+fi
+
+cd work
+./sign_image_v2.sh
+cp *-sign.bin ../output/
+cd ..
+zip -r -v resigned.zip output
